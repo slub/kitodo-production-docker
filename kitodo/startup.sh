2363db49c7f955cc2bb107e63aca6d617cf23a1d
Markus Weigelt <Markus.Weigelt@slub-dresden.de>
2022-10-11 12:06:45 +0200

    Improve makefile to use project specific docker-compose, add timezone mapping to docker-compose.yml, add log statements to startup.sh

--- kitodo/startup.sh
+++ kitodo/startup.sh
@@ -34,17 +34,20 @@ service rsyslog start
 echo "Wait for external database container."
 /wait-for-it.sh -t 0 ${DB_HOST}:${DB_PORT}
 
-if [ ! -f "/tmp/kitodo/all_files.sql" ]; then
-  cat /tmp/kitodo/kitodo.sql /tmp/kitodo/overwrites/sql/post_init.sql >/tmp/kitodo/all_files.sql
+ALL_FILES=/tmp/kitodo/all_files.sql
+# check if all_files.sh already exist else the initialization process has already been completed
+if [ ! -f "$ALL_FILES" ]; then
+  echo "Initialisation of environment"
+  cat /tmp/kitodo/kitodo.sql /tmp/kitodo/overwrites/sql/post_init.sql > $ALL_FILES
 
   TBL_QUERY_RESULT=$(echo "SHOW TABLES LIKE 'user'" | mysql -h "${DB_HOST}" -P "${DB_PORT}" -u ${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} -N)
 
   if [ "$TBL_QUERY_RESULT" != "user" ]; then # check if table already exits
     echo "Initialize database."
-    mysql -h "${DB_HOST}" -P "${DB_PORT}" -u ${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} </tmp/kitodo/all_files.sql
+    mysql -h "${DB_HOST}" -P "${DB_PORT}" -u ${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} < $ALL_FILES
   elif [ -s /tmp/kitodo/overwrites/sql/post_init.sql ]; then
     echo "Overwrite exiting database with sql file /tmp/kitodo/overwrites/sql/post_init.sql"
-    mysql -h "${DB_HOST}" -P "${DB_PORT}" -u ${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} </tmp/kitodo/overwrites/sql/post_init.sql
+    mysql -h "${DB_HOST}" -P "${DB_PORT}" -u ${DB_USER} --password=${DB_PASSWORD} ${DB_NAME} < /tmp/kitodo/overwrites/sql/post_init.sql
   fi
 
   echo "Initialize config modules directory."
@@ -54,7 +57,8 @@ if [ ! -f "/tmp/kitodo/all_files.sql" ]; then
     echo "Overwrite config modules directory data with data of /tmp/kitodo/overwrites/data."
     cp -pr /tmp/kitodo/overwrites/data/* /usr/local/kitodo/
   fi
-
+else
+    echo "Initialisation of environment already completed"
 fi
 
 /usr/bin/deploy.sh #deploy and start
