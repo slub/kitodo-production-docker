ec7ca05c16cb2badbbcd6f5fd8ca4ba3bb444146
Markus Weigelt <Markus.Weigelt@slub-dresden.de>
2023-03-29 10:31:09 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,9 +9,12 @@ chmod go-rwx /.ssh/*
 
 # Add ocrd manager as global and known_hosts if env exist
 if [ -n "$OCRD_MANAGER" ]; then
-  # export for using in scripts
   OCRD_MANAGER_HOST=${OCRD_MANAGER%:*}
   OCRD_MANAGER_PORT=${OCRD_MANAGER#*:}
+  
+  echo "Wait for ocrd manager ssh."
+  /wait-for-it.sh -t 0 ${OCRD_MANAGER_HOST}:${OCRD_MANAGER_PORT:-22}
+  
   OCRD_MANAGER_IP=$(nslookup $OCRD_MANAGER_HOST | grep 'Address\:' | awk 'NR==2 {print $2}')
 
   if test -e /etc/ssh/ssh_known_hosts; then
